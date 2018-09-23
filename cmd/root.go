36225f640d2e2d772f83f6170da2f907c7d76c49
fendijatmiko <jatmikofendi@gmail.com>
2018-09-23 09:14:10 +0700

    add execute function on cmd package

--- cmd/root.go
+++ cmd/root.go
@@ -39,3 +39,13 @@ var RootCmd = &cobra.Command{
 
 	initDB()
 }
+
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
