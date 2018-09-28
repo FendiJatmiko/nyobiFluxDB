318e1f16ee76945a6150fc0d4b5a25cfa205f803
Fendi D <jatmikofendi@gmail.com>
2018-09-28 15:05:06 +0700

    Fix/some syntax error (#4)
    * add init db
    
    * fix syntax error on init cobra and delete unused stuff

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,13 @@
-package cmd 
+package cmd
 
 import (
 	"context"
-	"github.com/influxdata/influxdb/client/v2"
+	"database/sql"
+	"log"
+	"net/http"
+	"sort"
+	"time"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -12,40 +17,35 @@ var (
 	dbPool *sql.DB
 
 	handler http.Handler
-	
 )
 
 var RootCmd = &cobra.Command{
-	Use: "InfluxDB Api",
+	Use:   "InfluxDB Api",
 	Short: "Influx",
-	Long: `nyobi InfluxDb with cobra and viper`,
+	Long:  `nyobi InfluxDb with cobra and viper`,
 	PreRun: func(cmd *cobra.Command, args []string) {
 		// Pre-Run specific context timeout.
 		//Note: Not sure if it's necessary to "config-ize" the timeout.
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 		defer cancel()
-	}
 
-	fmt.Println(`
+		fmt.Println(`
 		Server InfluxDB
 	`)
 
-	fmt.Println("Current server config:")
-	keys := viper.AllKeys()
-	sort.Strings(keys)
-	for _, key :=  range keys {
-		fmt.Println(fmt.Spintf("%s: %+v", key, viper.Get(key)))
-	}
+		fmt.Println("Current server config:")
+		keys := viper.AllKeys()
+		sort.Strings(keys)
+		for _, key := range keys {
+			fmt.Println(fmt.Spintf("%s: %+v", key, viper.Get(key)))
+		}
 
-	initDB()
-}
+		initDB()
 
+	},
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	if err := RootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	Run: func(cmd *cobra.Command, args []string) {
+		logger.Println("Listening to port:", viper.GetInt("app.port"))
+		http.ListenAndServe(fmt.Sprintf(":%d", viper.GetInt("app.port")), r)
+	},
 }
